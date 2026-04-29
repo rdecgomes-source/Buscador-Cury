2fc662d34e25a631726ee006d059c060f566211d
Cristiane G Araújo <SEUEMAIL@gmail.com>
2026-04-29 15:37:48 -0300

    Cria arquivo de dados separado

---
+++ dadosEmpreendimentos.js
@@ -0,0 +1,38 @@
+export const empreendimentos = [
+  {
+    nome: "Parque das Nações",
+    regiao: "Leste",
+    bairro: "Laguinho",
+    enderecoCompleto: "Não informado",
+    entrega: "",
+    unidadesDisponiveis: "236012",
+    tipologias: [
+      { tipo: "1 dormitório", preco: "A partir de R$ 236.012" },
+      { tipo: "2 dormitórios", preco: "A partir de R$ 335.708" }
+    ]
+  },
+  {
+    nome: "Mérito - Vila Mascote",
+    regiao: "Sul",
+    bairro: "Vila Mascote",
+    enderecoCompleto: "Não informado",
+    entrega: "",
+    unidadesDisponiveis: "517181",
+    tipologias: [
+      { tipo: "1 dormitório", preco: "A partir de R$ 294.914" },
+      { tipo: "2 dormitórios", preco: "A partir de R$ 517.181" }
+    ]
+  },
+  {
+    nome: "Cidade Mooca",
+    regiao: "Leste",
+    bairro: "Mooca",
+    enderecoCompleto: "Não informado",
+    entrega: "",
+    unidadesDisponiveis: "264343",
+    tipologias: [
+      { tipo: "1 dormitório", preco: "A partir de R$ 264.343" },
+      { tipo: "2 dormitórios", preco: "A partir de R$ 362.971" }
+    ]
+  }
+];
